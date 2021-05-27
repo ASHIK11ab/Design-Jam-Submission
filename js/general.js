e80f4ffcc1aa96fd32788366c1de4628893053ca
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2021-05-27 13:02:29 +0530

    update general.js

--- js/general.js
+++ js/general.js
@@ -4,8 +4,6 @@ const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function() {
   page_y_offset = window.pageYOffset;
-  console.log(`Page offset: ${page_y_offset}\nPrevious pos: ${previous_pos}`);
-  console.log(page_y_offset > previous_pos);
   if(page_y_offset > previous_pos)
     navbar.style.top = '-100px';
   else
