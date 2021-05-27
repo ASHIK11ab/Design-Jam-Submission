9582b6f4c20d5611df8088cef82afd261b8f59ed
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2021-05-27 23:29:58 +0530

    added bottom nav hiding on scroll down

--- js/general.js
+++ js/general.js
@@ -1,12 +1,23 @@
 let previous_pos = 0;
 let page_y_offset = 0;
 const navbar = document.querySelector('.navbar');
+const bottom_navbar = document.querySelector('.navbar-nav');
+const window_width = window.innerWidth;
+let bottom_nav_present = false;
 
 window.addEventListener('scroll', function() {
+  if(window_width <= 700) 
+    bottom_nav_present = true;
   page_y_offset = window.pageYOffset;
-  if(page_y_offset > previous_pos)
+  if(page_y_offset > previous_pos) {
     navbar.style.top = '-100px';
-  else
+    if(bottom_nav_present)
+      bottom_navbar.style.bottom = '-100px';
+  }
+  else {
+    if(bottom_navbar)
+      bottom_navbar.style.bottom = '0';
     navbar.style.top = '0';
+  }
   previous_pos = page_y_offset;
 });
